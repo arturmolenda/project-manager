3fa97a3790198c4a8c4a0da9077a30b5920bdf08
Artur Molenda <ar2rweb@gmail.com>
2021-01-04 10:32:25 +0100

    send email functionality

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+export default transporter;
