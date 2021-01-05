fa51503f65f19d296f9e4c563fb7552c5e0ba707
Artur Molenda <ar2rweb@gmail.com>
2021-01-05 21:00:56 +0100

    home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { CssBaseline } from '@material-ui/core';
+
+import Home from './components/pages/Home';
+import Layout from './components/layout/Layout';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <CssBaseline />
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </Layout>
+      </Router>
+    </>
+  );
 };
 
 export default App;
