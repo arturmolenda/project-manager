cc681cfbff239a0db352f96f2c63e1dd0680ea41
Artur Molenda <ar2rweb@gmail.com>
2021-01-06 14:25:25 +0100

    bring new changes to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { CssBaseline } from '@material-ui/core';
+import {
+  createMuiTheme,
+  CssBaseline,
+  MuiThemeProvider,
+} from '@material-ui/core';
 
 import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
 import Layout from './components/layout/Layout';
+import ParticlesBackground from './components/ParticlesBackground';
+import themeFile from './util/theme';
+
+const theme = createMuiTheme(themeFile);
 
 const App = () => {
   return (
-    <>
+    <MuiThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Layout>
           <Switch>
             <Route exact path='/'>
+              <ParticlesBackground />
               <Home />
+            </Route>
+            <Route exact path='/signin'>
+              <ParticlesBackground />
+              <Login />
+            </Route>
+            <Route exact path='/register'>
+              <ParticlesBackground />
+              <Register />
             </Route>
           </Switch>
         </Layout>
       </Router>
-    </>
+    </MuiThemeProvider>
   );
 };
 
