477255d8ec6e2b28f300a910f91b9235346fe5ae
Artur Molenda <ar2rweb@gmail.com>
2021-01-06 22:52:45 +0100

    correct code type to ObjectId

--- backend/models/emailSecret.js
+++ backend/models/emailSecret.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const emailSecretSchema = mongoose.Schema(
   {
     email: { type: String, required: true },
-    code: { type: String, required: true },
+    code: { type: mongoose.Types.ObjectId, required: true },
     dateCreated: { type: Date, default: Date.now(), expires: 1800 },
   },
   { timestamps: true }
