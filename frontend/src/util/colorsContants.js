2f6ecb9ddcbddef738e80652aaaf21e532900962
Artur Molenda <ar2rweb@gmail.com>
2021-01-08 09:40:43 +0100

    add colors constants

---
+++ frontend/src/util/colorsContants.js
@@ -0,0 +1,28 @@
+export const LABEL_COLORS = [
+  '#F94E4E',
+  '#28FA68',
+  '#49E4DA',
+  '#4A6DFF',
+  '#DED870',
+  '#E170FF',
+  '#984424',
+  '#FFF',
+  '#3C3735',
+];
+
+export const THEME_COLORS = [
+  '#00bcd4',
+  '#000',
+  '#FFEA6B',
+  '#00a8d5',
+  '#52a5a8',
+  '#830a0c',
+  '#5b3475',
+];
+export const BACKGROUND_COLORS = [
+  'linear-gradient(0deg, rgba(1,40,96,1) 0%, rgb(96 181 255) 100%)',
+  'linear-gradient(0deg, rgb(224 255 235) 0%, rgb(226, 155, 155) 100%',
+  'linear-gradient(0deg, rgba(10,10,10,1) 0%, rgba(1,28,68,1) 100%)',
+  'linear-gradient(0deg, rgba(65,224,226,1) 0%, rgba(92,117,213,1) 100%)',
+  'linear-gradient(0deg, rgba(19,20,27,1) 0%, rgba(35,39,65,1) 100%)',
+];
