3872cf0e3d1f6ce9c098bd0b02201861e5c457cb
Artur Molenda <ar2rweb@gmail.com>
2021-01-12 14:27:20 +0100

    better board container

--- frontend/src/components/dashboard/Board.js
+++ frontend/src/components/dashboard/Board.js
@@ -1,16 +1,40 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Lists from './lists/Lists';
 import Navbar from './navbar/Navbar';
 
+const useStyles = makeStyles(() => ({
+  boardContainer: {
+    height: '100vh',
+    position: 'relative',
+  },
+
+  board: {
+    height: '100vh',
+    display: 'grid',
+    flexWrap: 'nowrap',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    padding: '0 4px',
+    overflowX: 'auto',
+  },
+}));
+
 const Board = () => {
   const { project, labels, tasks } = useSelector(
     (state) => state.projectGetData
   );
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.boardContainer}>
       <Navbar />
-      <Lists />
+      <div className={classes.board}>
+        <Lists />
+      </div>
     </div>
   );
 };
