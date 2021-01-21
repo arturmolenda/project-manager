7a919e451a135da9d3f213d251d828f03f6e5dd1
Artur Molenda <ar2rweb@gmail.com>
2021-01-21 17:29:06 +0100

    shared components MenuHeader and DeleteMenu

---
+++ frontend/src/components/dashboard/shared/MenuHeader.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { Typography, Divider } from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
+
+const MenuHeader = ({ goBackHandle, handleClose, title }) => {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingBottom: 4,
+        }}
+      >
+        {goBackHandle ? (
+          <ArrowBackIosIcon
+            style={{
+              fontSize: '1rem',
+              cursor: 'pointer',
+              marginLeft: 10,
+            }}
+            onClick={goBackHandle}
+          />
+        ) : (
+          <CloseRoundedIcon
+            style={{
+              fontSize: '1.3rem',
+              cursor: 'pointer',
+              marginRight: 10,
+              visibility: 'hidden',
+            }}
+          />
+        )}
+        <Typography
+          variant='subtitle1'
+          style={{
+            overflowX: 'hidden',
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis',
+            padding: '0 15px',
+          }}
+        >
+          {title}
+        </Typography>
+        <CloseRoundedIcon
+          style={{
+            fontSize: '1.3rem',
+            cursor: 'pointer',
+            marginRight: 10,
+          }}
+          onClick={handleClose}
+        />
+      </div>
+      <Divider style={{ margin: '0 9px' }} />
+    </>
+  );
+};
+
+export default MenuHeader;
