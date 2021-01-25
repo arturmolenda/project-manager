4410d8850ed97b1416adf63700bf291b9da9cc75
Artur Molenda <ar2rweb@gmail.com>
2021-01-25 23:04:24 +0100

    add 'project-invite-users' and join-link socket handles

--- backend/socketControllers/socketProjectController.js
+++ backend/socketControllers/socketProjectController.js
@@ -1,4 +1,6 @@
 import Project from '../models/project.js';
+import Notification from '../models/notification.js';
+import mongoose from 'mongoose';
 
 export const socketProjectController = (io, socket) => {
   socket.on('project-title-update', async (data, callback) => {
@@ -6,5 +8,68 @@ export const socketProjectController = (io, socket) => {
     callback();
     socket.to(projectId).emit('project-title-updated', { title, projectId });
     await Project.updateOne({ _id: projectId }, { $set: { title: title } });
+  });
+
+  socket.on('project-invite-users', async (data, callback) => {
+    const { projectId, users } = data;
+
+    const project = await Project.findById(projectId);
+    if (users.length > 0) {
+      const promise = users.map(async (user) => {
+        const inviteNotification = new Notification({
+          type: 'Project Invitation',
+          projectId: projectId,
+          seenDate: null,
+          sender: socket.user._id,
+          recipient: user._id,
+        });
+        await inviteNotification.save();
+        project.users.push({
+          user: user._id,
+          permissions: 0,
+        });
+        io.to(user._id).emit('new-notification', {
+          notification: inviteNotification,
+        });
+      });
+      Promise.all(promise).then(async () => {
+        await project.save();
+        const newProjectUsers = await Project.findById(projectId).populate({
+          path: 'users.user',
+          select: 'username email profilePicture',
+        });
+        callback();
+        io.to(data.projectId).emit('users-updated', newProjectUsers.users);
+      });
+    }
+  });
+
+  socket.on('project-create-join-link', async (data) => {
+    const { projectId } = data;
+    const joinId = new mongoose.Types.ObjectId();
+    io.to(projectId).emit('project-join-link-updated', {
+      joinId,
+      joinIdActive: true,
+    });
+    await Project.updateOne(
+      { _id: projectId },
+      {
+        $set: {
+          joinId,
+          joinIdActive: true,
+        },
+      }
+    );
+  });
+  socket.on('project-disable-join-link', async (data) => {
+    const { projectId } = data;
+    io.to(projectId).emit('project-join-link-updated', {
+      joinId: null,
+      joinIdActive: false,
+    });
+    await Project.updateOne(
+      { _id: projectId },
+      { $set: { joinIdActive: false } }
+    );
   });
 };
