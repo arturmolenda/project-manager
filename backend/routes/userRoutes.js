32539ec16f8e1034d9b4b983c41a2250718da662
Artur Molenda <ar2rweb@gmail.com>
2021-01-26 17:51:30 +0100

    get notifications with user login, mark as seen and discard

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,9 @@ import {
   registerUser,
   resendEmail,
   findUsers,
+  markNotifications,
+  getNotifications,
+  discardNotification,
 } from '../controllers/userController.js';
 import { protect, permissionsOne } from '../middleware/authMiddleware.js';
 const router = express.Router();
@@ -14,7 +17,10 @@ router.post('/register', registerUser);
 router.post('/login', authUser);
 router.post('/confirm', confirmEmail);
 router.post('/resend', resendEmail);
+router.post('/markNotifications', markNotifications);
 router.post('/find/:projectId', protect, permissionsOne, findUsers);
+router.get('/notifications', protect, getNotifications);
 router.get('/', protect, getUserData);
+router.delete('/:notificationId', protect, discardNotification);
 
 export default router;
