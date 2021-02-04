eb60170f92098870b2f3b574d563edd0669db34d
arturmolenda <apmolenda@gmail.com>
2021-02-04 18:49:10 +0100

    add close icon and improve mobile responsiveness

--- frontend/src/components/dashboard/taskModal/TaskModal.js
+++ frontend/src/components/dashboard/taskModal/TaskModal.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { makeStyles, Modal } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
 import TaskHeader from './modalComponents/TaskHeader';
 import TaskDescription from './modalComponents/TaskDescription';
 import SideContent from './modalComponents/SideContent';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   container: {
+    position: 'relative',
+    overflow: 'auto',
+    borderRadius: 5,
     height: '90vh',
     maxWidth: 700,
     margin: '5vh auto',
@@ -16,14 +20,31 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    padding: '20px 10px 20px 20px',
+    padding: '25px 10px 20px 20px',
     backgroundColor: '#f4f5f7',
+    [theme.breakpoints.down(720)]: {
+      margin: '5vh 10px',
+    },
   },
   innerContainer: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
     marginTop: 10,
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      height: '100%',
+    },
+  },
+  closeIcon: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    padding: 3,
+    cursor: 'pointer',
+    '&:hover': {
+      color: theme.palette.primary.main,
+    },
   },
 }));
 
@@ -31,12 +52,14 @@ const TaskModal = ({ task, userPermissions }) => {
   const history = useHistory();
   const classes = useStyles();
 
+  const closeHandle = () => history.push(`/project/${task.projectId}`);
+
   return (
     <Modal
       disableEnforceFocus
       open={Boolean(task)}
       disableScrollLock
-      onClose={() => history.push(`/project/${task.projectId}`)}
+      onClose={closeHandle}
     >
       <div className={classes.container}>
         {task && (
@@ -53,6 +76,8 @@ const TaskModal = ({ task, userPermissions }) => {
             </div>
           </>
         )}
+
+        <CloseIcon className={classes.closeIcon} onClick={closeHandle} />
       </div>
     </Modal>
   );
