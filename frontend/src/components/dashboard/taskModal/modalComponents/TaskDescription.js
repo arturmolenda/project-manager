4daaf2c76d916e5559a33db1acf0501387a64085
arturmolenda <apmolenda@gmail.com>
2021-02-04 18:53:35 +0100

    add update task description functionality

--- frontend/src/components/dashboard/taskModal/modalComponents/TaskDescription.js
+++ frontend/src/components/dashboard/taskModal/modalComponents/TaskDescription.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
+import { useDispatch } from 'react-redux';
+import { taskFieldUpdate } from '../../../../redux/actions/projectActions';
+
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-import { makeStyles, Button } from '@material-ui/core';
+import { makeStyles, Button, ClickAwayListener } from '@material-ui/core';
+
+import Loader from '../../../Loader';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -46,68 +51,83 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const TaskDescription = ({
-  initialDescription,
-  projectId,
-  userPermissions,
-}) => {
+const TaskDescription = ({ taskId, task, projectId, userPermissions }) => {
+  const dispatch = useDispatch();
   const [descriptionEditOpen, setDescriptionEditOpen] = useState(false);
   const [description, setDescription] = useState('');
+  const [loading, setLoading] = useState(false);
   const classes = useStyles();
   useEffect(() => {
-    setDescription(initialDescription);
-  }, [initialDescription]);
+    setDescription(task.description);
+  }, [task.description]);
   const updateHandle = () => {
-    if (description !== initialDescription) {
+    if (description !== task.description) {
+      setLoading(true);
+      dispatch(
+        taskFieldUpdate(
+          task._id,
+          task.projectId,
+          description,
+          'description',
+          () => {
+            setLoading(false);
+            setDescriptionEditOpen(false);
+          }
+        )
+      );
     }
   };
-
   return (
-    <div className={classes.root}>
-      <CKEditor
-        editor={ClassicEditor}
-        config={{
-          toolbar: userPermissions >= 1 && [
-            'heading',
-            '|',
-            'bold',
-            'italic',
-            'blockQuote',
-            'link',
-            'numberedList',
-            'bulletedList',
-            'alignment',
-            'insertTable',
-            '|',
-            'undo',
-            'redo',
-          ],
-        }}
-        disabled={userPermissions < 1}
-        data={initialDescription}
-        onBlur={(event, editor) => setDescriptionEditOpen(false)}
-        onChange={(event, editor) => setDescription(editor.getData())}
-        onFocus={(event, editor) => setDescriptionEditOpen(true)}
-      />
-      {descriptionEditOpen && (
-        <div className={classes.actions}>
-          <Button
-            color='secondary'
-            onClick={() => setDescriptionEditOpen(false)}
-          >
-            Cancel
-          </Button>
-          <Button
-            color='primary'
-            variant='outlined'
-            disabled={description === initialDescription}
-            onClick={updateHandle}
-          >
-            Save
-          </Button>
-        </div>
-      )}
-    </div>
+    <ClickAwayListener
+      mouseEvent={'onMouseDown'}
+      onClickAway={() => !loading && setDescriptionEditOpen(false)}
+    >
+      <div className={classes.root}>
+        <CKEditor
+          editor={ClassicEditor}
+          config={{
+            toolbar: userPermissions >= 1 && [
+              'heading',
+              '|',
+              'bold',
+              'italic',
+              'blockQuote',
+              'link',
+              'numberedList',
+              'bulletedList',
+              'alignment',
+              'insertTable',
+              '|',
+              'undo',
+              'redo',
+            ],
+          }}
+          disabled={userPermissions < 1 || loading}
+          data={task.description}
+          onChange={(event, editor) => setDescription(editor.getData())}
+          onFocus={(event, editor) => setDescriptionEditOpen(true)}
+        />
+        {descriptionEditOpen && (
+          <div className={classes.actions}>
+            <Button
+              color='secondary'
+              onClick={() => setDescriptionEditOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              color='primary'
+              variant='outlined'
+              disabled={description === task.description || loading}
+              onClick={updateHandle}
+            >
+              Save
+              {loading && <Loader button />}
+            </Button>
+          </div>
+        )}
+      </div>
+    </ClickAwayListener>
   );
 };
 
