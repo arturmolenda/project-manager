bd056b6f236be0c6fce1adf63b1320924b2db31b
arturmolenda <apmolenda@gmail.com>
2021-02-09 21:34:05 +0100

    update Label import and localize task index and list index in useEffect

--- frontend/src/components/dashboard/taskModal/modalComponents/SideContent.js
+++ frontend/src/components/dashboard/taskModal/modalComponents/SideContent.js
@@ -7,7 +7,7 @@ import { makeStyles, Typography } from '@material-ui/core';
 import Archive from './sideComponents/Archive';
 import Copy from './sideComponents/Copy';
 import Deadline from './sideComponents/Deadline';
-import Label from './sideComponents/Label';
+import Label from './sideComponents/labels/Label';
 import ToDoList from './sideComponents/ToDoList';
 import Transfer from './sideComponents/Transfer';
 import Users from './sideComponents/users/Users';
@@ -45,16 +45,36 @@ const SideContent = ({ task, task: { archived, _id: taskId } }) => {
     lists: { lists },
   } = useSelector((state) => state.projectGetData);
   const [currentListId, setCurrentListId] = useState(false);
+  const [listIndex, setListIndex] = useState(false);
+  const [taskIndex, setTaskIndex] = useState(false);
   const classes = useStyles();
 
   useEffect(() => {
     if (!archived) {
-      const currentList = lists.find((list) => {
-        const foundList = list.tasks.find((x) => x._id === taskId);
-        return foundList && foundList._id;
-      });
-      if (currentList) setCurrentListId(currentList);
+      if (
+        listIndex > -1 &&
+        taskIndex > -1 &&
+        lists[listIndex] &&
+        lists[listIndex].tasks[taskIndex] &&
+        lists[listIndex].tasks[taskIndex]._id === taskId
+      ) {
+        // prevents unnecessary update
+        return;
+      } else {
+        const foundListIndex = lists.findIndex((list) => {
+          const foundTaskIndex = list.tasks.findIndex((x) => x._id === taskId);
+          if (foundTaskIndex > -1) {
+            setTaskIndex(foundTaskIndex);
+            return true;
+          } else return false;
+        });
+        if (foundListIndex > -1) {
+          setListIndex(foundListIndex);
+          setCurrentListId(lists[foundListIndex]._id);
+        }
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [lists, archived, taskId]);
 
   return (
@@ -64,7 +84,7 @@ const SideContent = ({ task, task: { archived, _id: taskId } }) => {
       </Typography>
       <div className={classes.buttonsContainer} style={{ marginBottom: 20 }}>
         <Users task={task} />
-        <Label task={task} />
+        <Label task={task} taskIndex={taskIndex} listIndex={listIndex} />
         <ToDoList task={task} />
         <Deadline task={task} />
       </div>
