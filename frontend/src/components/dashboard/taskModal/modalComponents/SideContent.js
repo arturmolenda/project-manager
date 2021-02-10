a8f73bdf636b4b85879b0311217e1ca05a4ba5a9
arturmolenda <apmolenda@gmail.com>
2021-02-10 22:37:32 +0100

    implement getTaskIndexes function and memoize component

--- frontend/src/components/dashboard/taskModal/modalComponents/SideContent.js
+++ frontend/src/components/dashboard/taskModal/modalComponents/SideContent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { useSelector } from 'react-redux';
+import { getTaskIndexes } from '../../../../util/utilFunctions';
 
 import { makeStyles, Typography } from '@material-ui/core';
 
@@ -12,6 +13,7 @@ import ToDoList from './sideComponents/ToDoList';
 import Transfer from './sideComponents/Transfer';
 import Users from './sideComponents/users/Users';
 import Watch from './sideComponents/Watch';
+import equal from 'fast-deep-equal';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -40,65 +42,59 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SideContent = ({ task, task: { archived, _id: taskId } }) => {
-  const {
-    lists: { lists },
-  } = useSelector((state) => state.projectGetData);
-  const [currentListId, setCurrentListId] = useState(false);
-  const [listIndex, setListIndex] = useState(false);
-  const [taskIndex, setTaskIndex] = useState(false);
-  const classes = useStyles();
+const SideContent = React.memo(
+  ({ task, task: { archived, _id: taskId } }) => {
+    const {
+      lists: { lists },
+    } = useSelector((state) => state.projectGetData);
+    const [currentListId, setCurrentListId] = useState(false);
+    const [listIndex, setListIndex] = useState(false);
+    const [taskIndex, setTaskIndex] = useState(false);
+    const classes = useStyles();
 
-  useEffect(() => {
-    if (!archived) {
-      if (
-        listIndex > -1 &&
-        taskIndex > -1 &&
-        lists[listIndex] &&
-        lists[listIndex].tasks[taskIndex] &&
-        lists[listIndex].tasks[taskIndex]._id === taskId
-      ) {
-        // prevents unnecessary update
-        return;
-      } else {
-        const foundListIndex = lists.findIndex((list) => {
-          const foundTaskIndex = list.tasks.findIndex((x) => x._id === taskId);
-          if (foundTaskIndex > -1) {
+    useEffect(() => {
+      if (!archived) {
+        getTaskIndexes(
+          lists,
+          taskIndex,
+          listIndex,
+          taskId,
+          (foundListIndex, foundTaskIndex) => {
+            setListIndex(foundListIndex);
             setTaskIndex(foundTaskIndex);
-            return true;
-          } else return false;
-        });
-        if (foundListIndex > -1) {
-          setListIndex(foundListIndex);
-          setCurrentListId(lists[foundListIndex]._id);
-        }
+            setCurrentListId(lists[foundListIndex]._id);
+          }
+        );
       }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lists, archived, taskId]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [lists, archived, taskId]);
 
-  return (
-    <div className={classes.container}>
-      <Typography variant='caption' className={classes.caption}>
-        ADD TO TASK
-      </Typography>
-      <div className={classes.buttonsContainer} style={{ marginBottom: 20 }}>
-        <Users task={task} />
-        <Label task={task} taskIndex={taskIndex} listIndex={listIndex} />
-        <ToDoList task={task} />
-        <Deadline task={task} />
+    return (
+      <div className={classes.container}>
+        <Typography variant='caption' className={classes.caption}>
+          ADD TO TASK
+        </Typography>
+        <div className={classes.buttonsContainer} style={{ marginBottom: 20 }}>
+          <Users task={task} />
+          <Label task={task} taskIndex={taskIndex} listIndex={listIndex} />
+          <ToDoList task={task} />
+          <Deadline task={task} />
+        </div>
+        <Typography variant='caption' className={classes.caption}>
+          ACTIONS
+        </Typography>
+        <div className={classes.buttonsContainer}>
+          <Copy task={task} />
+          <Watch task={task} />
+          <Transfer task={task} currentListId={currentListId} />
+          <Archive task={task} />
+        </div>
       </div>
-      <Typography variant='caption' className={classes.caption}>
-        ACTIONS
-      </Typography>
-      <div className={classes.buttonsContainer}>
-        <Copy task={task} />
-        <Watch task={task} />
-        <Transfer task={task} currentListId={currentListId} />
-        <Archive task={task} />
-      </div>
-    </div>
-  );
-};
+    );
+  },
+  (prevState, nextState) => {
+    return equal(prevState, nextState);
+  }
+);
 
 export default SideContent;
