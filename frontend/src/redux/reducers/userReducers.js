da0d56794e82da6118e3f3ca5f8578598553b12f
arturmolenda <apmolenda@gmail.com>
2021-02-21 22:24:25 +0100

    userPictureUpdateReducer redux

--- frontend/src/redux/reducers/userReducers.js
+++ frontend/src/redux/reducers/userReducers.js
@@ -16,6 +16,9 @@ import {
   USER_EMAIL_RESEND_FAIL,
   USER_NOTIFICATIONS_UPDATE,
   USER_REMOVED,
+  USER_PICTURE_UPDATE_REQUEST,
+  USER_PICTURE_UPDATE_SUCCESS,
+  USER_PICTURE_UPDATE_FAIL,
 } from '../constants/userConstants';
 import deepcopy from 'deepcopy';
 
@@ -59,6 +62,12 @@ export const userLoginReducer = (state = { loading: true }, action) => {
       );
       stateClone.projectsJoined.splice(projectIndex, 1);
       return { ...state, userInfo: stateClone };
+    }
+    case USER_PICTURE_UPDATE_SUCCESS: {
+      return {
+        ...state,
+        userInfo: { ...state.userInfo, profilePicture: action.payload },
+      };
     }
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
@@ -104,6 +113,19 @@ export const userEmailResendReducer = (state = {}, action) => {
     case USER_EMAIL_RESEND_SUCCESS:
       return { loading: false, success: action.payload };
     case USER_EMAIL_RESEND_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userPictureUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_PICTURE_UPDATE_REQUEST:
+      return { loading: true };
+    case USER_PICTURE_UPDATE_SUCCESS:
+      return { loading: false, success: true };
+    case USER_PICTURE_UPDATE_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
