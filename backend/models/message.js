7359c50a59ec73121a5bcc8c01f6bf8b43a1b788
arturmolenda <apmolenda@gmail.com>
2021-02-22 14:24:35 +0100

    update Message model schema

--- backend/models/message.js
+++ backend/models/message.js
@@ -3,12 +3,10 @@ import mongoose from 'mongoose';
 const messageSchema = mongoose.Schema(
   {
     message: { type: String, required: true },
-    username: { type: String, required: true },
-    userId: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
+    user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
     projectId: {
       type: mongoose.Types.ObjectId,
       required: true,
-      ref: 'Project',
     },
   },
   { timestamps: true }
