065502fbc47fc737e15fb9a215de00c855b09e5f
arturmolenda <apmolenda@gmail.com>
2021-03-06 00:09:15 +0100

    fix gmail blocking nodemailer

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,12 +1,15 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import smtpTransport from 'nodemailer-smtp-transport';
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
+const transporter = nodemailer.createTransport(
+  smtpTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  })
+);
 export default transporter;
